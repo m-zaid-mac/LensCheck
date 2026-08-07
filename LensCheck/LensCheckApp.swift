33b1e5cf8a29cf07d9163706727d674e92f68c91
mohammadzaid <zaid.m@northeastern.edu>
2026-08-06 20:41:18 -0400

    Initial Commit

---
+++ LensCheck/LensCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  LensCheckApp.swift
+//  LensCheck
+//
+//  Created by Mohammad Zaid on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LensCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
